8ce76fdc8aa862af5e4c8e2e1b36d051a6ea6528
Nathalia Lobato <n.e.m.q.v.5@gmail.com>
2018-01-27 05:26:11 -0200

    assets

---
+++ assets/js/contadorjavascript.js
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title>Sem Título</title>
+
+
+<script language="javascript"> 
+function setCookie(nome, valor, dias){ 
+diasms = (new Date()) .getTime() + 1000 * 3600 * 24 * dias; 
+dias = new Date(diasms); 
+expires = dias.toGMTString(); 
+document.cookie = escape(nome) + "=" + escape(valor) + "; expires=" + expires; 
+} 
+</script>
+
+
+</head>
+
+<body>
+
+<script language="javascript">
+if (!document.cookie){
+setCookie("cookie", "1", 365);
+document.write("<font face='verdana' size='1'>Suas Visitas : 1</font>");
+} else {
+var cont = document.cookie;
+var dividindo = cont.split("=");
+//document.write(dividindo[1]);
+var numero = parseInt(dividindo[1]);
+var soma = numero + 1;
+document.write("<font face='verdana' size='1'>Suas Visitas : " + soma + "</font>");
+setCookie("cookie", soma, 365);
+}
+</script> 
+
+
+
+</body>
+
+</html>
